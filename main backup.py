4fa69078ee735424990290b8d0691d6536abedc2
Armen Juhl <armenjuhl@gmail.com>
2017-07-30 20:52:41 -0700

    edits

--- main backup.py
+++ main backup.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
-#Armen Juhl
+from sqlalchemy.orm import relationship
+
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret" 
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:justblogit@localhost:8889/blogz'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz2:blogz@localhost:8889/blogz2'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'as9d8F7d98C3f7a'
@@ -18,7 +19,8 @@ class Blog(db.Model):
     body = db.Column(db.String(120))
     submitted = db.Column(db.Boolean)
     owner_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    def __init__(self, title, body, submitted,owner):
+
+    def __init__(self, title, body, submitted=True):
         self.title = title
         self.body = body
         self.submitted = submitted
@@ -28,7 +30,7 @@ class User(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(20))
-    blogs = db.relationship('Blog', backref = 'owner')
+    blogs = relationship("Blog", backref="user")
 
 
     def __init__(self, username, password, blogs):
@@ -51,51 +53,24 @@ def home():
 
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-    #blog_id = request.args.get('id')
     all_users = User.query.all()
-    return render_template('index.html', all_users=all_users)
+    blog_id = request.args.get('id')
+    return render_template('index.html', all_users=all_users, blog_id=blog_id)
 
 
     """blog_id = request.args.get('id')
     submitted_blogs = Blog.query.all()
     return render_template('index.html', submitted_blogs=submitted_blogs)"""
-@app.route('/blog', methods=["POST", "GET"])
-def list_blogs():
-    # define variable using database query
-    submitted_blogs = Blog.query.all()
-
-    # redirects to single blog entry when blog title is clicked
-    if "id" in request.args:
-        id = request.args.get('id')
-        entry = Blog.query.get(id)
-        
-        return render_template('blog.html', page_title="blog-post", title = entry.title, body = entry.body, owner = entry.owner)
-
-    # redirects to page showing all blog entries for a specific user when user name is clicked
-    if "user" in request.args:
-        owner_id = request.args.get('user')
-        user_submitted_blogs = Blog.query.filter_by(owner_id=owner_id)
-        username = User.query.filter_by(username=username)
-
-        return render_template('index.html', page_title = "User Contributions", user_submitted_blogs = user_submitted_blogs, user = username)
-
-    # displays template posts which displays all entries in descending order
-    return render_template('blog.html', page_title="blog", submitted_blogs = submitted_blogs)
 
 @app.route('/blog', methods=['POST', 'GET'])
 def show_posts(): 
-    if request.method == 'GET':
-        submitted_blogs = Blog.query.all()
     title = ''
     body = ''
-    if "id" in request.args:
-        id = request.args.get('id')
-        entry = Blog.query.get(id)
-    
+    if request.method == 'GET':
+        submitted_blogs = Blog.query.all()
         return render_template('blog.html', submitted_blogs=submitted_blogs, title=title, body=body)
-    return render_template('landing.html', page_title="blog-post", title = entry.title, body = entry.body, owner = entry.owner)
 
- 
+
 @app.route('/landing', methods =['GET'])
 def show_blog():
     blog_id = request.args.get('id')
@@ -113,44 +88,31 @@ def show_blog():
 #     else:
 #          return render_template('newpost.html', title='title', body='body')
 
-
-@app.route('/newpost', methods=["POST", "GET"])
-def add_entry():
-    
-    # render newpost template
-    if request.method == "GET":
-        return render_template('newpost.html', page_title="blog")
-
-    # define variables using form entries
-    title = request.form["title"]
-    body = request.form["body"]
-
-    # verification that title is filled in
-    if title == "":
-        title_error = "Blog entry must have a title."
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+    title = ''
+    body = ''
+    username = session['username']    
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']    
+        submitted = True        
+        owner_id = request.args.get(username)
+        #print("username:" + username)
+        user_id = User.query.filter_by(id=id).first()
+        #flash("user:", user)
+        #user_id = db.session.query(User.id).filter_by(username=request.form['username']).get()
+        #flash(user_id)
+        owner_id = User.query.filter_by(username=username).first()
+        #owner_id = user_id
+        newpost = Blog(title=title, body=body, submitted=True, owner_id=owner_id)
+        db.session.add(newpost)
+        db.session.commit()
+        # blog=newpost.id
+        # blogs=Blogs(title=title, body=body)
+        return redirect('/blog?id={0}'.format(blog))
     else:
-        title_error = "" 
-
-    # verification that body is filled in
-    if body == "":
-        body_error = "Blog entry must have content."
-    else:
-        body_error = ""
-    
-    # if there are no errors, adds new entry to the database and redirects user to entries template
-    if not title_error and not body_error:
-        if request.method == "POST":
-            submitted = True
-            owner= User.query.filter_by(username =session['username']).first()
-            newpost = Blog(title=title, body=body, submitted=True,owner=owner)
-            db.session.add(newpost)
-            db.session.commit()
-        
-        return render_template('blog.html', page_title = "confirmation", title = title, body = body, owner = owner)
-    else:
-        # re-renders newpost template with appropriate error messages if errors exist
-        return render_template('newpost.html', page_title = "blog", title = title, 
-            title_error = title_error, body = body, body_error = body_error)
+        return render_template('newpost.html', title='title', body='body', username=username)
 
 
 """
@@ -221,7 +183,16 @@ def signup():
 def logout():
     del session['username']
     return redirect('/login')
+# @app.route('/delete-entry', methods=['POST'])
+# def delete_entry():
 
+#     blog_id = int(request.form['entry-id'])
+#     entry = Blog.query.filter_by(title)
+#     Blog.completed = False
+#     db.session.delete(entry)
+#     db.session.commit()
+
+#     return redirect('/')
 
 
 if __name__ == '__main__':
